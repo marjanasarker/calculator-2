769ca543d157ebe02dcc27dee9ad00e72d80e100
Marjana <msarker86@gmail.com>
2021-03-05 00:24:04 -0500

    Add to existing file

--- calculator.py
+++ calculator.py
@@ -25,10 +25,10 @@ while True:
         break
     
     
-    elif len(tokens)<3:
+    elif len(tokens)==2:
         num1=tokens[1]
         num2=0
-    else:
+    elif len(tokens)==3:
         num1=tokens[1]
         num2=tokens[2]
         
@@ -40,5 +40,13 @@ while True:
             result=power(float(num1), float (num2))
         elif operator=="*":
             result=multiply(float(num1), float (num2))
+        elif operator=="/":
+            result=divide(float(num1), float (num2))
+        elif operator=="sq"
+            result=square(float(num1))
+        elif operator=="cube"
+            result=cube(float(num1))
+        elif operator=="mod"
+            result=mod(float(num1), float(num2))
     print (result)
 #except ValueError:
